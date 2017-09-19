bb038bb639e387e1518f9adbe39e167cc0ef5e72
Tong Zhang <zhangt@frib.msu.edu>
2017-09-19 15:26:31 -0400

    Version: 0.2.1.

--- main/__init__.py
+++ main/__init__.py
@@ -1,7 +1,7 @@
 from archappl.client import *
 
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Tong Zhang <zhangt@frib.msu.edu>'
 
 __doc__ ="""archappl: Python interface of Archiver Appliance."""
