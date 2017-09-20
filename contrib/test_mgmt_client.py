1e53c907fd99fcb4a49e7dd8f717f2d49427ea69
Tong Zhang <zhangt@frib.msu.edu>
2017-09-20 09:18:08 -0400

    Clean up.

--- contrib/test_mgmt_client.py
+++
@@ -1,9 +0,0 @@
-from archappl.client import ArchiverMgmtClient
-
-
-client = ArchiverMgmtClient()
-client.url = 'http://127.0.0.1:17665'
-
-all_pvs = client.get_all_pvs()
-print(all_pvs)
-# [u'TST:fakeGaussianNoise', u'TST:gaussianNoise', u'TST:uniformNoise']
