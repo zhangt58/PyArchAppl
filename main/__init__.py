50a0a5350b851b8c1764b57a8fabe4f9da26cfd7
Tong Zhang <zhangt@frib.msu.edu>
2020-11-09 17:01:35 -0500

    MNT: Bump up version to 0.3.0.

--- main/__init__.py
+++ main/__init__.py
@@ -1,7 +1,7 @@
 from archappl.client import *
 
 
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 __author__ = 'Tong Zhang <zhangt@frib.msu.edu>'
 
 __doc__ ="""archappl: Python interface of Archiver Appliance."""
--- setup.py
+++ setup.py
@@ -3,18 +3,20 @@ from setuptools import setup
 
 setup(
         name='pyarchappl',
-        version='0.2.2',
+        version='0.3.0',
         description='Python interface to Archiver Appliance',
         author='Tong Zhang',
         author_email='zhangt@frib.msu.edu',
         packages=['archappl.admin',
                   'archappl.data',
                   'archappl.client',
+                  'archappl.contrib',
                   'archappl'],
         package_dir={
             'archappl.admin': 'main/mgmt',
             'archappl.data' : 'main/data',
             'archappl.client': 'main/client',
+            'archappl.contrib': 'main/contrib',
             'archappl': 'main'
         },
 )
