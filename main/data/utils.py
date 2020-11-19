af1897659803e9dd97f333cc1b1fd8cb1ad96f5e
Tong Zhang <zhangt@frib.msu.edu>
2020-11-19 10:31:01 -0500

    API: Properly handle daylight saving time zone issue.
    - datetime_with_timezone(): ensures datetime object always has correct tzinfo
    - is_dst(): check if datetime object is in DST zone or not
    - parse_dt(): always calculate the datetime distance with UTC zone, then convert
      back to original zone if necessary (e.g. if reference datetime is not in UTC)

--- main/data/utils.py
+++ main/data/utils.py
@@ -7,7 +7,7 @@ import tzlocal
 import pytz
 
 LOCAL_ZONE = tzlocal.get_localzone()
-LOCAL_ZONE_NAME = LOCAL_ZONE.zone
+LOCAL_ZONE_NAME = LOCAL_ZONE.zone # America/New_York
 
 
 def iso_to_epoch(s):
@@ -52,9 +52,10 @@ def epoch_to_iso(ts, tz="-05:00"):
 
 def datetime_with_timezone(date_time, time_zone=None):
     """Function to represent datetime object *date_time* with timezone info.
-    If input *date_time* does have tzinfo, and if *time_zone* is defined, convert *date_time* to
-    defined timezone, otherwise does nothing.
-    If input *date_time* does not have tzinfo, set with defined *time_zone*.
+    If input *date_time* does have tzinfo, and if *time_zone* is defined, convert
+    *date_time* to defined timezone, otherwise does nothing.
+    If input *date_time* does not have tzinfo, set with defined *time_zone*, no
+    changes of date time.
 
     Parameters
     ----------
@@ -82,10 +83,22 @@ def datetime_with_timezone(date_time, time_zone=None):
     >>> print(dt1.hour - dt0_w_tz.hour) # UTC if ahead of EST by 5 hours
     5
     >>> dt2 = datetime(2020, 11, 10, 1, 12, 3, 456789) # arbitrary datetime but w/o zone
+    >>> # present dt2 at local zone
+    >>> dt2_local = datetime_with_timezone(dt2)
+    >>> print(dt2_local)
+    2020-11-10 01:12:03.456789-05:00
     >>> # amend it with 'UTC' zone, only affect tz
-    >>> dt2_w_utc = datetime_with_timezone(dt2, time_zone='UTC')
-    >>> print(dt2_w_utc)
+    >>> dt2_at_utc = datetime_with_timezone(dt2, time_zone='UTC')
+    >>> print(dt2_at_utc) # only append timezone +00:00
     2020-11-10 01:12:03.456789+00:00
+    >>> # represent dt2 at UTC zone
+    >>> dt2_as_utc = datetime_with_timezone(dt2_local, time_zone='UTC')
+    >>> print(dt2_as_utc)
+    2020-11-10 06:12:03.456789+00:00
+    >>> print((dt2_at_utc - dt2_local).total_seconds() / 3600)
+    -5.0
+    >>> print((dt2_as_utc - dt2_local).total_seconds())
+    0.0
     """
     if time_zone is None:
         _timezone = LOCAL_ZONE
@@ -94,18 +107,48 @@ def datetime_with_timezone(date_time, time_zone=None):
 
     if date_time.tzinfo is not None:
         if time_zone is not None:
-            _dt = date_time.astimezone(_timezone)
+            _dt1 = date_time.astimezone(_timezone)
+            _dt = _timezone.localize(datetime(_dt1.year, _dt1.month, _dt1.day,
+                                              _dt1.hour, _dt1.minute, _dt1.second,
+                                              _dt1.microsecond))
         else:
-            _dt = date_time
+            _dt1 = date_time
+            _dt = date_time.tzinfo.localize(datetime(
+                                              _dt1.year, _dt1.month, _dt1.day,
+                                              _dt1.hour, _dt1.minute, _dt1.second,
+                                              _dt1.microsecond))
     else:
-        _dt = date_time.replace(tzinfo=_timezone)
+        _dt1 = date_time
+        _dt = _timezone.localize(datetime(_dt1.year, _dt1.month, _dt1.day,
+                                          _dt1.hour, _dt1.minute, _dt1.second,
+                                          _dt1.microsecond))
     return _dt
 
 
+def is_dst(date_time):
+    """Return if *date_time* is in daylight saving time period.
+
+    Examples
+    --------
+    >>> from datetime import datetime
+    >>>
+    >>> t0 = datetime(2016, 11, 2, 12, 34, 56, 123456)  # local time at America/New_York
+    >>> print(is_dst(t0))
+    True
+    >>> print(is_dst(t0.astimezone(pytz.timezone('UTC'))))
+    False
+    """
+    if date_time.tzinfo is None:
+        return bool(pytz.timezone(LOCAL_ZONE_NAME).localize(date_time).dst())
+    else:
+        return bool(date_time.dst())
+
+
 # see phantasy.parse_dt
-def parse_dt(dt, ref_datetime=None, epoch=None, timezone=None, **kws):
-    """Parse delta time defined by *dt*, which is approching plain English,
-    e.g. '1 hour and 30 mins ago', return datetime object with tzinfo at *timezone*.
+def parse_dt(dt, ref_datetime=None, epoch=None):
+    """Parse delta time defined by *dt*, which is presenting in plain English,
+    e.g. '1 hour and 30 mins ago', return datetime object at the same timezone.
+    The if tzinfo is missing in *ref_datetime*, use local timezone.
 
     Parameters
     ----------
@@ -118,17 +161,9 @@ def parse_dt(dt, ref_datetime=None, epoch=None, timezone=None, **kws):
         ends with 'before', or begins with 'after', e.g. '5 mins before',
         '1 hour and 30 mins before', 'after 15 seconds', etc.
     ref_datetime : datetime
-        Datetime object, with *timezone* info.
+        Datetime object, with tzinfo, if not defined, use current local datetime.
     epoch : bool
         If return date time as seconds since Epoch.
-    timezone : str
-        Name of time zone, default is local time zone, e.g. 'EST',
-        see pytz.all_timezones.
-
-    Keyword Arguments
-    -----------------
-    align_tz: bool
-        If True, returned date time with the tzinfo as reference date time.
 
     Warning
     -------
@@ -137,10 +172,10 @@ def parse_dt(dt, ref_datetime=None, epoch=None, timezone=None, **kws):
     Returns
     -------
     ret : datetime
-        Datetime object.
+        Datetime object with the same tzinfo of reference datetime.
 
-    See Also:
-    ---------
+    See Also
+    --------
     datetime_with_timezone
 
     Examples
@@ -148,23 +183,25 @@ def parse_dt(dt, ref_datetime=None, epoch=None, timezone=None, **kws):
     >>> dt0 = datetime_with_timezone(datetime.now())
     >>> dt1 = parse_dt("1 hour before", dt0)
     >>> dt2 = parse_dt("after 1 hour", dt0)
-    >>> assert dt0 - dt0 == dt2 - dt1
+    >>> assert dt0 - dt1 == dt2 - dt0
+    >>> assert (dt0 - dt1).total_seconds() == 3600.0
     >>> dt_str = '1 month, 2 weeks, 4 hours, 7 mins and 10 secs before'
-    >>> print(parse_dt(dt_str, datetime(2016, 12, 10, 12, 34, 56, 123456))) # local tz EST
-    2016-10-27 08:27:46.123456-05:00
-    >>> # east daylight saving
-    >>> print(parse_dt(dt_str, datetime(2016, 12, 10, 12, 34, 56, 123456), align_tz=True))
+    >>> print(parse_dt(dt_str, datetime(2016, 12, 10, 12, 34, 56, 123456)))
+    >>> # local tz EST --> datetime at DST
     2016-10-27 09:27:46.123456-04:00
-    >>>
+    >>> t0 = datetime(2016, 11, 5, 23, 0, 0, 0)  # DST
+    >>> print(datetime_with_timezone(t0))
+    2016-11-05 23:00:00-04:00
+    >>> parse_dt("after 3 hours", t0)
+    2016-11-06 01:00:00-05:00
     """
-    if timezone is None:
-        tz = LOCAL_ZONE
-    else:
-        tz = pytz.timezone(timezone)
     if ref_datetime is None:
-        timenow = datetime.now(tz=tz)
+        ref_dt = datetime_with_timezone(datetime.now())
     elif isinstance(ref_datetime, datetime):
-        timenow = ref_datetime.astimezone(tz)
+        if ref_datetime.tzinfo is None:
+            ref_dt = datetime_with_timezone(ref_datetime)
+        else:
+            ref_dt = ref_datetime
     else:
         raise TypeError("Invalid datetime variable.")
 
@@ -187,18 +224,17 @@ def parse_dt(dt, ref_datetime=None, epoch=None, timezone=None, **kws):
 
     dt = relativedelta.relativedelta(**dt_dict)
 
+    ref_dt_as_utc = datetime_with_timezone(ref_dt, time_zone='UTC')
     if is_retro:
-        _datetime = timenow - dt
+        _datetime_as_utc = ref_dt_as_utc - dt
     else:
-        _datetime = timenow + dt
+        _datetime_as_utc = ref_dt_as_utc + dt
+    _datetime = datetime_with_timezone(_datetime_as_utc, time_zone=ref_dt.tzinfo.zone)
     if epoch:
         r = _datetime.timestamp
     else:
         r = _datetime
-    if kws.get('align_tz', False):
-        return r.astimezone(ref_datetime.tzinfo)
-    else:
-        return r
+    return datetime_with_timezone(r)
 
 
 if __name__ == "__main__":
