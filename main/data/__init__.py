8da1dcd68db1ace3d857fb38c362028ce0bc1d55
Tong Zhang <zhangt@frib.msu.edu>
2020-11-19 10:35:10 -0500

    MNT: Add is_dst() to archappl.data namespace.

--- main/data/__init__.py
+++ main/data/__init__.py
@@ -3,10 +3,12 @@ from .utils import iso_to_epoch
 from .utils import epoch_to_iso
 from .utils import parse_dt
 from .utils import datetime_with_timezone
+from .utils import is_dst
 
 FRIBArchiverDataClient = ArchiverDataClient(
     "http://epicsarchiver0.ftc:17668")
 
 __all__ = ["ArchiverDataClient", "FRIBArchiverDataClient",
            "iso_to_epoch", "epoch_to_iso", "parse_dt",
-           "datetime_with_timezone"]
+           "datetime_with_timezone",
+           "is_dst"]
