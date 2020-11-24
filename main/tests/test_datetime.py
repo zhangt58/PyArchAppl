5483fde8a714e2786ddb25589472f422db9c4a81
Tong Zhang <zhangt@frib.msu.edu>
2020-11-24 16:29:05 -0500

    TST: Fix the timezone issue.
    - github workflow runtime is UTC, so set it force.
    - datetime() with tzinfo does not work sometime.
      - e.g. datetime(2016, 11, 5, 23, 0, 0, 0, tzinfo=pytz.timezone('America/New_York')), should wrap with datetime_with_timezone():
      datetime_with_timezone(datetime(2016, 11, 5, 23, 0, 0, 0), time_zone='America/New_York')

--- main/tests/test_datetime.py
+++ main/tests/test_datetime.py
@@ -8,33 +8,34 @@ from archappl.data import datetime_with_timezone
 from archappl.data.utils import standardize_datetime
 from archappl.data.utils import LOCAL_ZONE_NAME
 
+US_NY_ZONE_NAME = 'America/New_York'
+US_NY_ZONE = pytz.timezone(US_NY_ZONE_NAME)
 
-T0_DST = datetime(2016, 11, 5, 23, 0, 0, 0,
-                  tzinfo=pytz.timezone('America/New_York'))  # DST
-T1_EST = datetime(2016, 11, 6 ,6, 0, 0, 0,
-                  tzinfo=pytz.timezone('America/New_York'))
+T0 = datetime(2016, 11, 5, 23, 0, 0, 0)
+T1 = datetime(2016, 11, 6 ,6, 0, 0, 0)
 
-print(datetime_with_timezone(T0_DST))
+T0_DST = datetime_with_timezone(datetime(2016, 11, 5, 23, 0, 0, 0), time_zone=US_NY_ZONE_NAME)
+T1_EST = datetime_with_timezone(datetime(2016, 11, 6 ,6, 0, 0, 0), time_zone=US_NY_ZONE_NAME)
 
 
-def test_func_datetime_with_timezone():
-    """Test `datetime_with_timezone()`.
+def test_func_datetime_with_timezone1():
+    """Test `datetime_with_timezone()`, DST.
     """
     tt0 = datetime_with_timezone(T0_DST)
     tt1 = datetime_with_timezone(T1_EST)
 
-    assert tt0.tzinfo.zone == LOCAL_ZONE_NAME
+    assert tt0.tzinfo.zone == US_NY_ZONE_NAME
     assert bool(tt0.dst()) == True
     assert bool(tt0.dst()) == is_dst(T0_DST)
 
-    assert tt1.tzinfo.zone == LOCAL_ZONE_NAME
+    assert tt1.tzinfo.zone == US_NY_ZONE_NAME
     assert bool(tt1.dst()) == False
     assert bool(tt1.dst()) == is_dst(T1_EST)
 
-    t0_at_utc = datetime_with_timezone(T0_DST, time_zone='UTC')
+    t0_at_utc = datetime_with_timezone(T0, time_zone='UTC')
     assert (tt0 - t0_at_utc).total_seconds() / 3600 == 4.0
 
-    t1_at_utc = datetime_with_timezone(T1_EST, time_zone='UTC')
+    t1_at_utc = datetime_with_timezone(T1, time_zone='UTC')
     assert (tt1 - t1_at_utc).total_seconds() / 3600 == 5.0
 
     t0_as_utc = datetime_with_timezone(tt0, time_zone='UTC')
@@ -42,6 +43,26 @@ def test_func_datetime_with_timezone():
 
     t1_as_utc = datetime_with_timezone(tt1, time_zone='UTC')
     assert (tt1 - t1_as_utc).total_seconds() == 0.0
+
+
+def test_func_datetime_with_timezone2():
+    """Test `datetime_with_timezone()`, local zone may not fall into DST.
+    """
+    tt0 = datetime_with_timezone(T0)
+    tt1 = datetime_with_timezone(T1)
+
+    assert tt0.tzinfo.zone == LOCAL_ZONE_NAME
+    assert tt1.tzinfo.zone == LOCAL_ZONE_NAME
+
+    # inteprete datetime at US_NY_ZONE_NAME
+    tt0 = datetime_with_timezone(T0, time_zone=US_NY_ZONE_NAME)
+    tt1 = datetime_with_timezone(T1, time_zone=US_NY_ZONE_NAME)
+
+    assert tt0.tzinfo.zone == US_NY_ZONE_NAME
+    assert bool(tt0.dst()) == True
+
+    assert tt1.tzinfo.zone == US_NY_ZONE_NAME
+    assert bool(tt1.dst()) == False
 
 
 def test_func_is_dst():
@@ -94,9 +115,9 @@ def test_standardize_datetime():
     """
     t0 = datetime_with_timezone(T0_DST)
     t0_as_utc = datetime_with_timezone(t0, 'UTC')
-    datetimetuple1 = (2016, 11, 5, 23)  # 2016-11-05 23:00:00
+    datetimetuple1 = (2016, 11, 5, 23, 0, 0)  # 2016-11-05 23:00:00
 
-    d_str1 = standardize_datetime(datetimetuple1)
+    d_str1 = standardize_datetime(datetimetuple1, time_zone=US_NY_ZONE_NAME)
     assert d_str1 == f"{t0_as_utc.year:4d}-{t0_as_utc.month:02d}-{t0_as_utc.day:02d}T{t0_as_utc.hour:02d}:{t0_as_utc.minute:02d}:{t0_as_utc.second:02d}.{int(t0_as_utc.microsecond/1000):03d}Z"
 
     assert d_str1 == standardize_datetime(T0_DST)
