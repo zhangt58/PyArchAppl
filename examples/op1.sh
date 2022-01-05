09fd84fca88e350f6742de4f883d2a8f4a3e3a38
Tong Zhang <zhangt@frib.msu.edu>
2022-01-05 13:54:05 -0500

    MNT: Clean up.

--- examples/op1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# post
-curl -H "Content-Type: application/json" -XGET -s \
-    "http://epicsarchiver0.ftc:17668/retrieval/data/getData.json?pv=FE_MEBT:BPM_D1056:XPOS_RD&from=2020-10-09T12:00:00.000000-04:00&to=2020-10-09T12:15:00.000000-04:00"
